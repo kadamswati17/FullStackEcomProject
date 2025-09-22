366802c2683e339521eb49393bbe7a3649fe4ead
swati kadam <swatimarotikadam@gmail.com>
2025-09-22 12:26:09 +0530

    admin and customer navbar added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth-service';
+import { UserStorageService } from '../services/storage/user-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -42,12 +43,16 @@ export class LoginComponent {
     const userName = this.loginForm.get('email')!.value;
     const email = this.loginForm.get('email')!.value;
     const password = this.loginForm.get('password')!.value;
-    console.log(userName);
-    console.log(password);
+
 
     this.authService.login(userName, password).subscribe(
       (response) => {
-        this.snackBar.open('login successful', 'ok', { duration: 5000 })
+        if (UserStorageService.isAdminLoggedIn()) {
+          this.router.navigateByUrl('/admin/dashboard');
+        }
+        else if (UserStorageService.isCustomerLoggedIn()) {
+          this.router.navigateByUrl('/customer/dashboard');
+        }
       },
       (error) => {
         this.snackBar.open('bad credentials', 'error', { duration: 5000 })
--- src/app/services/storage/user-storage.service.ts
+++ src/app/services/storage/user-storage.service.ts
@@ -11,12 +11,47 @@ export class UserStorageService {
   constructor() { }
 
   public saveToken(token: string): void {
-    window.sessionStorage.removeItem(TOKEN);
     window.sessionStorage.setItem(TOKEN, token);
   }
 
-  public saveUser(user): void {
-    window.sessionStorage.removeItem(USER);
+  public saveUser(user: any): void {
     window.sessionStorage.setItem(USER, JSON.stringify(user));
   }
+
+  static getToken(): string | null {
+    return window.sessionStorage.getItem(TOKEN);
+  }
+
+  static getUser(): any {
+    const user = window.sessionStorage.getItem(USER);
+    return user ? JSON.parse(user) : null;
+  }
+
+  static getUserId(): string | null {
+    const user = this.getUser();
+    return user ? user.userId : null;
+  }
+
+  static getUserRole(): string | null {
+    const user = this.getUser();
+    return user ? user.role : null;
+  }
+
+  static isAdminLoggedIn(): boolean {
+    const token = this.getToken();
+    const role = this.getUserRole();
+    return token != null && role === 'ADMIN';
+  }
+
+  static isCustomerLoggedIn(): boolean {
+    const token = this.getToken();
+    const role = this.getUserRole();
+    return token != null && role === 'CUSTOMER';
+  }
+
+  static signOut(): void {
+    window.sessionStorage.removeItem(TOKEN);
+    window.sessionStorage.removeItem(USER);
+  }
+
 }
