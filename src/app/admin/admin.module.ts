5812ac2607b831c50be7af0ae5136fb65418a7ce
swati kadam <swatimarotikadam@gmail.com>
2025-09-23 14:06:45 +0530

    description  api added

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,16 +4,26 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularMaterialDemo } from '../AngularMaterialDemo';
+import { PostCategoryComponent } from './components/post-category/post-category.component';
+
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostCategoryComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AngularMaterialDemo
   ]
 })
 export class AdminModule { }
--- src/app/services/storage/user-storage.service.ts
+++ src/app/services/storage/user-storage.service.ts
@@ -1,3 +1,73 @@
+// import { Injectable } from '@angular/core';
+
+// const TOKEN = 'ecom-token';
+// const USER = 'ecom-user';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UserStorageService {
+
+//   constructor() { }
+
+//   public saveToken(token: string): void {
+//     window.sessionStorage.removeItem(TOKEN);
+//     window.sessionStorage.setItem(TOKEN, token);
+//   }
+
+//   public saveUser(user): void {
+//     window.sessionStorage.removeItem(USER);
+//     window.sessionStorage.setItem(USER, JSON.stringify(user));
+//   }
+
+//   static getToken(): string {
+//     return localStorage.getItem(TOKEN);
+//   }
+
+//   static getUser(): any {
+//     return JSON.parse(localStorage.getItem(USER));
+//   }
+
+//   static getUserId(): string {
+//     const user = this.getUser();
+//     if (user) {
+//       return '';
+//     }
+//     return user.userId;
+//   }
+
+
+//   static getUserRole(): string {
+//     const user = this.getUser();
+//     if (user) {
+//       return '';
+//     }
+//     return user.role;
+//   }
+
+//   static isAdminLoggedIn(): boolean {
+//     if (this.getToken === null) {
+//       return false;
+//     }
+//     const role: string = this.getUserRole();
+//     return role === 'ADMIN';
+//   }
+
+//   static isCustomerLoggedIn(): boolean {
+//     if (this.getToken === null) {
+//       return false;
+//     }
+//     const role: string = this.getUserRole();
+//     return role === 'CUSTOMER';
+//   }
+
+//   static signOut(): void {
+//     window.localStorage.removeItem(TOKEN);
+//     window.localStorage.removeItem(USER);
+//   }
+
+// }
+
 import { Injectable } from '@angular/core';
 
 const TOKEN = 'ecom-token';
